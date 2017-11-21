706043d93f1a3692000eb45ce70491b92910ffdc
gagan0183 <g.deepsingh1@gmail.com>
2017-11-21 21:51:33 +0530

    interpolateprovider

--- hello/app.js
+++ hello/app.js
@@ -1,29 +1,26 @@
-var app = angular.module('app', []);
-app.controller('FirstController', function($scope) {
-    $scope.counter = 0;
-    $scope.add = function(amount) {
-        $scope.counter += amount;
-    }
-    $scope.subtract = function(amount) {
-        $scope.counter -= amount;
-    }
-});
+var email = angular.module('emailParser', []);
 
-app.controller('ParseController', function($scope, $parse) {
-    $scope.$watch('expr', function(newVal, oldVal, scope) {
-        if(newVal !== oldVal) {
-            var parseFun = $parse(newVal);
-            console.log(parseFun);
-            $scope.parseValue = parseFun(scope);
+email.config(['$interpolateProvider', function($interpolateProvider) {
+       $interpolateProvider.startSymbol('__');
+       $interpolateProvider.endSymbol('__'); 
+    }])
+    .factory('EmailParser', ['$interpolate', function($interpolate) {
+        return {
+            parse: function(text, context) {
+                var template = $interpolate(text);
+                return template(context);
+            }
         }
-    });
-});
+    }]);
 
-app.controller('InterpolateController', function($scope, $interpolate) {
+
+var app = angular.module('app', ['emailParser']);
+app.controller('EmailController', ['$scope', 'EmailParser', function($scope, EmailParser) {
     $scope.$watch('emailBody', function(body) {
         if(body) {
-            var template = $interpolate(body);
-            $scope.previewText = template({ to: $scope.to });
+            $scope.previewText = EmailParser.parse(body, {
+                to: $scope.to
+            });
         }
     });
-});
+}]);
